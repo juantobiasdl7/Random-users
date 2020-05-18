fa3fc671f846686db9efd81df6df39e4c1390100
Juan Tobias <juantobiasdl7@gmail.com>
2020-05-18 17:26:06 -0500

    I partially finished this project.

--- app.js
+++ app.js
@@ -24,12 +24,45 @@ async function getRandomUser() {
     addData(newUser);
 }
 
+// Double everyones money
+function doubleMoney() {
+    data = data.map(user => {
+        return { ...user, money: user.money * 2 }
+    });
+
+    updateDOM();
+}
+
+// Sort users by richest
+function sortByRichest() {
+    data.sort((a,b) => b.money - a.money);
+
+    updateDOM();
+}
+
+// Filter only millionaires
+function showMillionaires() {
+    data = data.filter(user => user.money > 1000000);
+
+    updateDOM();
+}
+
 // add new obj to data arr
 function addData(obj) {
     data.push(obj);
 
     updateDOM();
 }
+
+// Calculate Wealth
+function calculateWealth() {
+    const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+
+    const wealthEl = document.createElement('div');
+    wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(wealth)}</strong></h3>`;
+    main.appendChild(wealthEl);
+}
+
 
 // Update DOM
 function updateDOM(providedData = data) {
@@ -51,3 +84,7 @@ function formatMoney(number) {
 
 // Event listeners
 addUserBtn.addEventListener('click', getRandomUser);
+doubleBtn.addEventListener('click', doubleMoney);
+sortBtn.addEventListener('click', sortByRichest);
+showMillionairesBtn.addEventListener('click', showMillionaires);
+calculateWealthBtn.addEventListener('click', calculateWealth);
